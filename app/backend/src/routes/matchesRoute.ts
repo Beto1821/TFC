14d6244c5657b0f6a4e33241761147878a89719f
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-25 18:32:59 -0300

    req20

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -4,7 +4,5 @@ import MatchesController from '../controllers/Matches';
 const matchesRoute = Router();
 
 matchesRoute.get('/matches', MatchesController.getAll);
-// matchesRoute.get('/teams/:id/finish', MatchesController.getIdFinish);
-// matchesRoute.get('/teams/:id', MatchesController.getId);
 
 export default matchesRoute;
